42e22954a55ff0ef6b10a8d9318455cfe8e41e7f
Abdennour Kouider Araibi <AE1219@student.jamk.fi>
2024-02-05 15:38:22 +0200

    New commit

---
+++ Assets/Scripts/Game UI/MainMenu.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void NewGame() => SceneManager.LoadSceneAsync("MainGameScene");
+
+    public void QuitGame() => Application.Quit();
+}
